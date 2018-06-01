09fab18f85b3b55297eacbc91fce2d471582336f
wangyafeng <wangyafeng.159@163.com>
2018-06-01 10:14:10 +0800

    change

--- src/main/java/com/art1001/supply/controller/base/BaseController.java
+++ src/main/java/com/art1001/supply/controller/base/BaseController.java
@@ -1,8 +1,6 @@
 package com.art1001.supply.controller.base;
 
 import lombok.extern.slf4j.Slf4j;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 
 /**
