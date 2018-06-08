d307df6589f6b8857fe897a5ec78912367586f90
wangyafeng <wangyafeng.159@163.com>
2018-06-08 14:34:18 +0800

    添加新的controller类

---
+++ src/main/java/com/art1001/supply/controller/ProjectController.java
@@ -0,0 +1,15 @@
+package com.art1001.supply.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+
+/**
+ * 项目控制器
+ */
+@Controller
+@Slf4j
+public class ProjectController {
+
+
+
+}
---
+++ src/main/java/com/art1001/supply/controller/TaskController.java
@@ -0,0 +1,12 @@
+package com.art1001.supply.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+
+/**
+ * 任务控制器，关于任务的操作
+ */
+@Controller
+@Slf4j
+public class TaskController {
+}
