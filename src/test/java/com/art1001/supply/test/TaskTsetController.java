cba140bfdcd1cf9d3ac1c7c31c13a28e90823a93
何少华 <heshaohua@art1001.com>
2018-06-12 16:31:26 +0800

    测试代码删除

--- src/test/java/com/art1001/supply/test/TaskTsetController.java
+++ src/test/java/com/art1001/supply/test/TaskTsetController.java
@@ -25,210 +25,210 @@ import java.util.List;
  **/
 public class TaskTsetController extends TestBase{
 
-    @Resource
-    private TaskService taskService;
-
-    @Resource
-    private TagService tagService;
-
-    @Resource
-    private TaskLogService taskLogService;
-
-    @Resource
-    private TaskMemberService taskMemberService;
-
-    /**
-     * 添加任务的测试方法
-     */
-    @Test
-    public void addTask(){
-        Task task = new Task();
-        task.setTaskId("1000");
-        task.setUpdateTime(System.currentTimeMillis());
-        task.setCreateTime(System.currentTimeMillis());
-        task.setTaskDel(0);
-        task.setTaskStatus("0");
-        task.setEndTime(System.currentTimeMillis());
-        task.setExecutor("1");
-        task.setLevel(1);
-        task.setMemberId("1");
-        task.setParentId("1");
-        task.setPriority("1");
-        task.setProjectId("1");
-        task.setTaskType("1");
-        task.setUpdatePerson("2");
-        JSONObject jsonObject = new JSONObject();
-        String id = "";
-//        try {
-//            //获取当前session中的用户,如果有用户则为该任务的创建人
-//            id = String.valueOf(SecurityUtils.getSubject().getSession().getAttribute("id"));
-//            if(id == null || id.equals("")){
-//                jsonObject.put("msg","用户登陆超时，请重新登陆！");
-//                jsonObject.put("result","0");
-//                return jsonObject;
-//            }
-            task.setTaskId(id);
-            //保存任务信息到数据库
-            //taskService.saveTask(startTime, endTime, remindTime, task);
-            jsonObject.put("msg","添加任务成功!");
-//        } catch (Exception e){
-//            jsonObject.put("error","任务添加失败!");
+//    @Resource
+//    private TaskService taskService;
+//
+//    @Resource
+//    private TagService tagService;
+//
+//    @Resource
+//    private TaskLogService taskLogService;
+//
+//    @Resource
+//    private TaskMemberService taskMemberService;
+//
+//    /**
+//     * 添加任务的测试方法
+//     */
+//    @Test
+//    public void addTask(){
+//        Task task = new Task();
+//        task.setTaskId("1000");
+//        task.setUpdateTime(System.currentTimeMillis());
+//        task.setCreateTime(System.currentTimeMillis());
+//        task.setTaskDel(0);
+//        task.setTaskStatus("0");
+//        task.setEndTime(System.currentTimeMillis());
+//        task.setExecutor("1");
+//        task.setLevel(1);
+//        task.setMemberId("1");
+//        task.setParentId("1");
+//        task.setPriority("1");
+//        task.setProjectId("1");
+//        task.setTaskType("1");
+//        task.setUpdatePerson("2");
+//        JSONObject jsonObject = new JSONObject();
+//        String id = "";
+////        try {
+////            //获取当前session中的用户,如果有用户则为该任务的创建人
+////            id = String.valueOf(SecurityUtils.getSubject().getSession().getAttribute("id"));
+////            if(id == null || id.equals("")){
+////                jsonObject.put("msg","用户登陆超时，请重新登陆！");
+////                jsonObject.put("result","0");
+////                return jsonObject;
+////            }
+//            task.setTaskId(id);
+//            //保存任务信息到数据库
+//            //taskService.saveTask(startTime, endTime, remindTime, task);
+//            jsonObject.put("msg","添加任务成功!");
+////        } catch (Exception e){
+////            jsonObject.put("error","任务添加失败!");
+////        }
+//
+//    }
+//
+//    /**
+//     * 移动任务至其他组的测试方法
+//     */
+//    @Test
+//    public void mobileTask(){
+//       Task t = new Task();
+//       t.setTaskId("11111");
+//       t.setTaskMenuId("1");
+//       TaskMenuVO oldTaskMenuVO = new TaskMenuVO();
+//        oldTaskMenuVO.setProjectId("1");
+//        oldTaskMenuVO.setProjectName("项目A");
+//        oldTaskMenuVO.setTaskGroupId("1");
+//        oldTaskMenuVO.setTaskGroupName("分组A");
+//        oldTaskMenuVO.setTaskMenuId("1");
+//        oldTaskMenuVO.setTaskMenuName("菜单A");
+//        TaskMenuVO newTaskMenuVO = new TaskMenuVO();
+//        newTaskMenuVO.setProjectId("2");
+//        newTaskMenuVO.setProjectName("项目B");
+//        newTaskMenuVO.setTaskGroupId("2");
+//        newTaskMenuVO.setTaskGroupName("分组B");
+//        newTaskMenuVO.setTaskMenuId("2");
+//        newTaskMenuVO.setTaskMenuName("菜单B");
+//
+//       try {
+//            //修改该任务的任务组编号
+//           TaskLogVO taskLogVO = taskService.mobileTask(t, oldTaskMenuVO, newTaskMenuVO);
+//           System.out.println(taskLogVO.getResult());
+//           System.out.println(taskLogVO.getContent());
+//       } catch (ServiceException e){
+//           System.out.println(e.getMessage());
 //        }
-
-    }
-
-    /**
-     * 移动任务至其他组的测试方法
-     */
-    @Test
-    public void mobileTask(){
-       Task t = new Task();
-       t.setTaskId("11111");
-       t.setTaskMenuId("1");
-       TaskMenuVO oldTaskMenuVO = new TaskMenuVO();
-        oldTaskMenuVO.setProjectId("1");
-        oldTaskMenuVO.setProjectName("项目A");
-        oldTaskMenuVO.setTaskGroupId("1");
-        oldTaskMenuVO.setTaskGroupName("分组A");
-        oldTaskMenuVO.setTaskMenuId("1");
-        oldTaskMenuVO.setTaskMenuName("菜单A");
-        TaskMenuVO newTaskMenuVO = new TaskMenuVO();
-        newTaskMenuVO.setProjectId("2");
-        newTaskMenuVO.setProjectName("项目B");
-        newTaskMenuVO.setTaskGroupId("2");
-        newTaskMenuVO.setTaskGroupName("分组B");
-        newTaskMenuVO.setTaskMenuId("2");
-        newTaskMenuVO.setTaskMenuName("菜单B");
-
-       try {
-            //修改该任务的任务组编号
-           TaskLogVO taskLogVO = taskService.mobileTask(t, oldTaskMenuVO, newTaskMenuVO);
-           System.out.println(taskLogVO.getResult());
-           System.out.println(taskLogVO.getContent());
-       } catch (ServiceException e){
-           System.out.println(e.getMessage());
-        }
-        return ;
-    }
-
-    /**
-     * 将任务 (移入回收站/回复) 的测试方法
-     * @return
-     */
-    @Test
-    public void moveToRecycleBin() {
-        String taskId = "11111";
-        String taskDel = "1";
-        try {
-            //int result = taskService.moveToRecycleBin(taskId, taskDel);
-        } catch (ServiceException e) {
-            System.out.println(e.getMessage());
-        }
-    }
-
-    /**
-     * 永久删除任务的测试方法
-     */
-    @Test
-    public void delTask(){
-        String taskId = "11111";
-        try {
-            int result = taskService.deleteTaskByTaskId(taskId);
-            System.out.println(result);
-        } catch (ServiceException e){
-            System.out.println(e.getMessage());
-        }
-
-    }
-
-    /**
-     * 完成任务(2=完成 1=未完成)
-     */
-    @Test
-    public void changeTaskStatus(){
-        String taskId = "11111";
-        String taskStatus = "1";
-        try {
-            //int result = taskService.changeTaskStatus(taskId,taskStatus);
-           // System.out.println(result);
-        } catch (ServiceException e){
-            System.out.println(e.getMessage());
-        }
-    }
-
-    /**
-     * 修改该任务信息的测试方法
-     */
-    @Test
-    public void updateTask(){
-        Task task = new Task();
-        try {
-            taskService.updateTask(task);
-        } catch (ServiceException e){
-            System.out.println(e.getMessage());
-        }
-    }
-
-    /**
-     * 更新任务时间( 开始 / 结束 / 提醒)
-     */
-    @Test
-    public void updateTaskTime(){
-        String startTime = "1995-3-22";
-        String endTime = "2018-9-9";
-        String taskId = "11111";
-        String remindTime = "2018-6-9";
-        try {
-            //int result = taskService.updateTaskTime(taskId,startTime,endTime,remindTime);
-        } catch (Exception e){
-            System.out.println(e.getMessage());
-        }
-    }
-
-    @Test
-    public void addTask1(){
-        Task task = new Task();
-        Project project = new Project();
-        project.setProjectName("A项目");
-        project.setStartTime(System.currentTimeMillis());
-        String[] memberId = {"4","21"};
-        String id = "11111";
-        try {
-            task.setMemberId(id);
-            //保存任务信息到数据库
-            taskService.saveTask(memberId,project,task);
-            List<TaskLog> taskLogAllList = taskLogService.findTaskLogAllList();
-            for (TaskLog log: taskLogAllList) {
-                System.out.println(log.getContent());
-            }
-        } catch (Exception e){
-        }
-    }
-
-    @Test
-    public void addTags(){
-        String taskId = "11111";
-        String[] oldTags = {"1","2"};
-        Tag tag = new Tag();
-        tag.setTagName("121212");
-        try {
-            int result = tagService.saveTag(tag,oldTags,taskId);
-        } catch (Exception e){
-            System.out.println(e.getMessage());
-        }
-    }
-
-    @Test
-    public void addRaly(){
-        Task task = null;
-        File file = new File();
-        file.setFileName("何少华");
-        Schedule schedule = null;
-        Share share = null;
-        TaskMember taskMember = new TaskMember();
-        String taskId = "1";
-        TaskLogVO taskLogVO = taskMemberService.saveTaskMember(task,file,share,schedule,taskMember,taskId);
-    }
+//        return ;
+//    }
+//
+//    /**
+//     * 将任务 (移入回收站/回复) 的测试方法
+//     * @return
+//     */
+//    @Test
+//    public void moveToRecycleBin() {
+//        String taskId = "11111";
+//        String taskDel = "1";
+//        try {
+//            //int result = taskService.moveToRecycleBin(taskId, taskDel);
+//        } catch (ServiceException e) {
+//            System.out.println(e.getMessage());
+//        }
+//    }
+//
+//    /**
+//     * 永久删除任务的测试方法
+//     */
+//    @Test
+//    public void delTask(){
+//        String taskId = "11111";
+//        try {
+//            int result = taskService.deleteTaskByTaskId(taskId);
+//            System.out.println(result);
+//        } catch (ServiceException e){
+//            System.out.println(e.getMessage());
+//        }
+//
+//    }
+//
+//    /**
+//     * 完成任务(2=完成 1=未完成)
+//     */
+//    @Test
+//    public void changeTaskStatus(){
+//        String taskId = "11111";
+//        String taskStatus = "1";
+//        try {
+//            //int result = taskService.changeTaskStatus(taskId,taskStatus);
+//           // System.out.println(result);
+//        } catch (ServiceException e){
+//            System.out.println(e.getMessage());
+//        }
+//    }
+//
+//    /**
+//     * 修改该任务信息的测试方法
+//     */
+//    @Test
+//    public void updateTask(){
+//        Task task = new Task();
+//        try {
+//            taskService.updateTask(task);
+//        } catch (ServiceException e){
+//            System.out.println(e.getMessage());
+//        }
+//    }
+//
+//    /**
+//     * 更新任务时间( 开始 / 结束 / 提醒)
+//     */
+//    @Test
+//    public void updateTaskTime(){
+//        String startTime = "1995-3-22";
+//        String endTime = "2018-9-9";
+//        String taskId = "11111";
+//        String remindTime = "2018-6-9";
+//        try {
+//            //int result = taskService.updateTaskTime(taskId,startTime,endTime,remindTime);
+//        } catch (Exception e){
+//            System.out.println(e.getMessage());
+//        }
+//    }
+//
+//    @Test
+//    public void addTask1(){
+//        Task task = new Task();
+//        Project project = new Project();
+//        project.setProjectName("A项目");
+//        project.setStartTime(System.currentTimeMillis());
+//        String[] memberId = {"4","21"};
+//        String id = "11111";
+//        try {
+//            task.setMemberId(id);
+//            //保存任务信息到数据库
+//            taskService.saveTask(memberId,project,task);
+//            List<TaskLog> taskLogAllList = taskLogService.findTaskLogAllList();
+//            for (TaskLog log: taskLogAllList) {
+//                System.out.println(log.getContent());
+//            }
+//        } catch (Exception e){
+//        }
+//    }
+//
+//    @Test
+//    public void addTags(){
+//        String taskId = "11111";
+//        String[] oldTags = {"1","2"};
+//        Tag tag = new Tag();
+//        tag.setTagName("121212");
+//        try {
+//            int result = tagService.saveTag(tag,oldTags,taskId);
+//        } catch (Exception e){
+//            System.out.println(e.getMessage());
+//        }
+//    }
+//
+//    @Test
+//    public void addRaly(){
+//        Task task = null;
+//        File file = new File();
+//        file.setFileName("何少华");
+//        Schedule schedule = null;
+//        Share share = null;
+//        TaskMember taskMember = new TaskMember();
+//        String taskId = "1";
+//        TaskLogVO taskLogVO = taskMemberService.saveTaskMember(task,file,share,schedule,taskMember,taskId);
+//    }
 
 
 }
