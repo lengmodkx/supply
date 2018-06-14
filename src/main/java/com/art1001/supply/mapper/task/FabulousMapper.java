22b02cdfa977eeee39aa4ef2ad25d6f4bb812191
何少华 <heshaohua@art1001.com>
2018-06-14 11:42:41 +0800

    修改FabulousMapper.xml文件

---
+++ src/main/java/com/art1001/supply/mapper/task/FabulousMapper.java
@@ -0,0 +1,12 @@
+package com.art1001.supply.mapper.task;
+
+/**
+ * @author heshaohua
+ * @Title:
+ * @Description: 得赞数的dao接口
+ * @date 2018/6/14 11:40
+ */
+public interface FabulousMapper {
+
+
+}
