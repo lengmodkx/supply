81210dd5518410d88cb2909a42d39cfbb6a3075f
lijian <2439676969@qq.com>
2018-06-25 17:30:26 +0800

    抽取弹框

--- src/main/resources/static/js/file.js
+++ src/main/resources/static/js/file.js
@@ -105,11 +105,7 @@ $(function () {
                 layui.use('form', function(){
                     var form = layui.form;
 
-                    //监听提交
-                    form.on('submit(formDemo)', function(data){
-                        layer.msg(JSON.stringify(data.field));
-                        return false;
-                    });
+                    form.render();
                 });
 
             }
