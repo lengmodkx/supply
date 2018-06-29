314be05531f8370e297851c9d80d923fba995215
wangyafeng <wangyafeng.159@163.com>
2018-06-29 17:56:34 +0800

    修改项目

---
+++ src/main/java/com/art1001/supply/config/WebSocketStompConfig.java
@@ -0,0 +1,38 @@
+package com.art1001.supply.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebSocketStompConfig implements WebSocketMessageBrokerConfigurer {
+
+    /**
+     * 注册stomp的端点
+     */
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        // 允许使用socketJs方式访问，访问点为webSocketServer，允许跨域
+        // 在网页上我们就可以通过这个链接
+        // http://localhost:8080/webSocketServer
+        // 来和服务器的WebSocket连接
+        registry.addEndpoint("/webSocketServer").setAllowedOrigins("*").withSockJS();
+    }
+
+    /**
+     * 配置信息代理
+     */
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+        // 订阅Broker名称 /topic 代表发布广播，即群发 /queue 代表点对点，即发指定用户
+        registry.enableSimpleBroker("/queue", "/topic");
+        // 全局使用的消息前缀（客户端订阅路径上会体现出来）
+        registry.setApplicationDestinationPrefixes("/app","/project","/task");
+        // 点对点使用的订阅前缀（客户端订阅路径上会体现出来），不设置的话，默认也是/user/
+        // registry.setUserDestinationPrefix("/user/");
+    }
+
+}
---
+++ src/main/resources/static/js/project.js
@@ -0,0 +1,52 @@
+// 建立连接对象（还未发起连接）
+var socket = new SockJS("http://localhost:8080/webSocketServer");
+// 获取 STOMP 子协议的客户端对象
+var stompClient = Stomp.over(socket);
+
+// 向服务器发起websocket连接并发送CONNECT帧
+stompClient.connect({},
+    function connectCallback(frame) {
+        // 连接成功时（服务器响应 CONNECTED 帧）的回调方法
+        console.log("连接成功");
+        subscribe1();
+    },
+    function errorCallBack(error) {
+        // 连接失败时（服务器响应 ERROR 帧）的回调方法
+        console.log("连接失败");
+    }
+);
+
+
+//订阅消息
+function subscribe1() {
+    stompClient.subscribe('/topic/subscribe', function (response) {
+        var returnData = JSON.parse(response.body);
+        dealMessage(JSON.parse(returnData.responseMessage));
+    });
+}
+
+
+
+//将消息显示在网页上
+function dealMessage(project) {
+
+    var li = '<li class="layui-col-md3 layui-anim layui-anim-scaleSpring"\n' +
+        '                    style="background-image:url('+IMAGE_SERVER + project.projectCover+');"\n' +
+        '                    onclick="javascript:projectClick(\''+project.projectId+'\')"\n' +
+        '                    id="'+project.projectId+'">\n' +
+        '                    <div class="describe boxsizing">\n' +
+        '                        <div>\n' +
+        '                            <div class="describe-title">'+project.projectName+'</div>\n' +
+        '                            <span class="tools">\n' +
+        '                                <i class="layui-icon layui-icon-edit " style="font-size: 18px; color: #eeeeee;" title="打开项目设置"\n' +
+        '                                   onclick="javascript:setting(\''+project.projectId+'\')"></i>\n' +
+        '                                <i class="layui-icon layui-icon-rate-solid star" style="font-size: 18px; color: #eeeeee;" title="收藏项目"\n' +
+        '                                   onclick="javascript:projectCollect(\''+project.projectId+'\')"></i>\n' +
+        '                            </span>\n' +
+        '                        </div>\n' +
+        '                        <p>'+project.projectDes+'</p>\n' +
+        '                    </div>\n' +
+        '                </li>';
+
+    $(".partake ul").append(li);
+}
