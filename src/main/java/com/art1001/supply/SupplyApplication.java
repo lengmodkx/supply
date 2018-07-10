8c09387038a4c828b373c89579ae637980904ffd
wangyafeng <wangyafeng.159@163.com>
2018-07-10 14:06:43 +0800

    修改项目

--- src/main/java/com/art1001/supply/SupplyApplication.java
+++ src/main/java/com/art1001/supply/SupplyApplication.java
@@ -2,15 +2,22 @@ package com.art1001.supply;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 
 @ComponentScan(basePackages ="com.art1001.supply")
 @ServletComponentScan
 @SpringBootApplication
-public class SupplyApplication {
+public class SupplyApplication  extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(SupplyApplication.class, args);
+    }
+
+    @Override protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        // 注意这里要指向原先用main方法执行的Application启动类
+        return builder.sources(SupplyApplication.class);
     }
 }
