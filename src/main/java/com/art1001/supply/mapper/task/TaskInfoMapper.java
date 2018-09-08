45c229d5ea29f8d0b1273355fcf7fb27dda5ffbc
chaiwei <793262255@qq.com>
2018-09-08 14:10:48 +0800

    // 统计数据查询mapper

---
+++ src/main/java/com/art1001/supply/mapper/task/TaskInfoMapper.java
@@ -0,0 +1,16 @@
+package com.art1001.supply.mapper.task;
+
+/**
+ * 模块
+ *
+ * @author lujing
+ * @date 2018-09-08 14:02
+ */
+public interface TaskInfoMapper {
+
+    /**
+     * 查询总任务数
+     * @return int
+     */
+    int selectTaskCount();
+}
