5e3711ddec821d8e1ddbeee6d2af273d4b2f732d
chaiwei <793262255@qq.com>
2018-09-08 11:42:54 +0800

    // Statistic 更新统计页面chart数据

--- src/main/java/com/art1001/supply/controller/StatisticsController.java
+++ src/main/java/com/art1001/supply/controller/StatisticsController.java
@@ -2,19 +2,25 @@ package com.art1001.supply.controller;
 
 import com.art1001.supply.controller.base.BaseController;
 import com.art1001.supply.entity.statistics.Statistics;
+import com.art1001.supply.entity.statistics.TaskCondition;
 import com.art1001.supply.entity.user.UserEntity;
 import com.art1001.supply.service.project.ProjectService;
+import com.art1001.supply.service.task.TaskInfoService;
 import com.art1001.supply.service.task.TaskService;
 import com.art1001.supply.service.user.UserService;
 import com.art1001.supply.shiro.ShiroAuthenticationManager;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.annotation.Resource;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Controller
 @Slf4j
@@ -28,6 +34,9 @@ public class StatisticsController extends BaseController {
 
     @Resource
     private UserService userService;
+
+    @Resource
+    private TaskInfoService taskInfoService;
     /**
      * 加载任务统计页面的信息
      * @param projectId 项目id
@@ -48,5 +57,26 @@ public class StatisticsController extends BaseController {
     public String statisticsDetail(){
         return "tk-statistics-details";
     }
+
+    /*
+     *总量概览
+     **/
+
+    @RequestMapping("/chart/{chartId}")
+    @ResponseBody
+    public Map statisticsChart(@PathVariable String chartId, TaskCondition taskCondition){
+        Map map=new HashMap<>(16);
+
+        if (chartId!=null && !"".equals(chartId)){
+            try {
+                map = this.taskInfoService.selectTask(taskCondition,chartId);
+            } catch (Exception e) {
+                map=null;
+                e.printStackTrace();
+            }
+        }
+        return  map;
+    }
+
 
 }
