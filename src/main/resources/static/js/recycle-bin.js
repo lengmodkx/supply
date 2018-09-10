aa7c1d32374f165940c4cb8bf40c57e42d1c4c73
何少华 <heshaohua@art1001.com>
2018-09-10 19:42:48 +0800

    任务

--- src/main/resources/static/js/recycle-bin.js
+++ src/main/resources/static/js/recycle-bin.js
@@ -373,3 +373,11 @@ $('.forever-delete-btn').click(function () {
         }
     });
 });
+
+/**
+ * 点击x 关闭
+ */
+$('.layui-icon-close').click(function () {
+    var index = parent.layer.getFrameIndex(window.name); //先得到当前iframe层的索引
+    parent.layer.close(index); //再执行关闭
+});
