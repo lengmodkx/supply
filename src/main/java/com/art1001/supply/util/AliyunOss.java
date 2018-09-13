565e607dcee7d095bb73cd71fb2f86f81e7ad977
wangyafeng <wangyafeng.159@163.com>
2018-09-13 10:32:10 +0800

    修改文件相关

--- src/main/java/com/art1001/supply/util/AliyunOss.java
+++ src/main/java/com/art1001/supply/util/AliyunOss.java
@@ -6,15 +6,20 @@ import com.aliyun.oss.event.ProgressEventType;
 import com.aliyun.oss.event.ProgressListener;
 import com.aliyun.oss.model.*;
 import com.art1001.supply.common.Constants;
+import com.art1001.supply.exception.SystemException;
 import lombok.extern.slf4j.Slf4j;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.ByteArrayInputStream;
-import java.io.File;
-import java.io.InputStream;
+import java.io.*;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.net.URLEncoder;
 import java.util.List;
+import java.util.zip.Adler32;
+import java.util.zip.CheckedOutputStream;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
 
 @Slf4j
 public class AliyunOss {
@@ -349,17 +354,66 @@ public class AliyunOss {
         ossClient.shutdown();
     }
 
-    public void downloadzip(List<com.art1001.supply.entity.file.File> fileList, HttpServletResponse response){
+    public static void downloadzip(List<com.art1001.supply.entity.file.File> fileList, HttpServletResponse response, HttpServletRequest request) throws Exception{
         OSSClient ossClient = new OSSClient(endpoint, accessKeyId, accessKeySecret);
+        String fileName = "chat_file.zip";
+        File zipFile = File.createTempFile("chat_file", ".zip");
+        FileOutputStream f = new FileOutputStream(zipFile);
+        /**
+         * 作用是为任何OutputStream产生校验和
+         * 第一个参数是制定产生校验和的输出流，第二个参数是指定Checksum的类型 （Adler32（较快）和CRC32两种）
+         */
+        CheckedOutputStream csum = new CheckedOutputStream(f, new Adler32());
+        // 用于将数据压缩成Zip文件格式
+        ZipOutputStream zos = new ZipOutputStream(csum);
+        fileList.forEach(file -> {
+            OSSObject ossObject = ossClient.getObject(bucketName, file.getFileUrl());
+            InputStream inputStream = ossObject.getObjectContent();
+            try {
+                zos.putNextEntry(new ZipEntry(file.getFileName()));
+                int bytesRead = 0;
+                // 向压缩文件中输出数据
+                while((bytesRead=inputStream.read())!=-1){
+                    zos.write(bytesRead);
+                }
+                inputStream.close();
+                zos.closeEntry(); // 当前文件写完，定位为写入下一条项目
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        });
+        zos.close();
+        String header = request.getHeader("User-Agent").toUpperCase();
+        if (header.contains("MSIE") || header.contains("TRIDENT") || header.contains("EDGE")) {
+            fileName = URLEncoder.encode(fileName, "utf-8");
+            fileName = fileName.replace("+", "%20");    //IE下载文件名空格变+号问题
+        } else {
+            fileName = new String(fileName.getBytes(), "ISO8859-1");
+        }
 
+        response.reset();
+        response.setContentType("text/plain");
+        response.setContentType("application/octet-stream; charset=utf-8");
+        response.setHeader("Location", fileName);
+        response.setHeader("Cache-Control", "max-age=0");
+        response.setHeader("Content-Disposition", "attachment; filename=" + fileName);
 
-
+        FileInputStream fis = new FileInputStream(zipFile);
+        BufferedInputStream buff = new BufferedInputStream(fis);
+        BufferedOutputStream out=new BufferedOutputStream(response.getOutputStream());
+        byte[] car=new byte[1024];
+        int l=0;
+        while (l < zipFile.length()) {
+            int j = buff.read(car, 0, 1024);
+            l += j;
+            out.write(car, 0, j);
+        }
+        // 关闭流
+        fis.close();
+        buff.close();
+        out.close();
+        ossClient.shutdown();
+        // 删除临时文件
+        zipFile.delete();
     }
-
-
-
-
-
-
-
 }
