3db55ad850539edbd4033cf1c036736653571166
wangyafeng <wangyafeng.159@163.com>
2018-09-21 16:19:33 +0800

    添加角色

--- src/main/java/com/art1001/supply/service/role/RoleService.java
+++ src/main/java/com/art1001/supply/service/role/RoleService.java
@@ -1,9 +1,15 @@
 package com.art1001.supply.service.role;
-
-
 import com.art1001.supply.entity.role.Role;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.baomidou.mybatisplus.extension.service.IService;
 
 public interface RoleService extends IService<Role> {
-
+    /**
+     * 分页查询
+     * @param current 当前页
+     * @param size 每页多少条数据
+     * @param role 查询条件
+     * @return
+     */
+    Page<Role> selectListPage(long current, long size, Role role);
 }
