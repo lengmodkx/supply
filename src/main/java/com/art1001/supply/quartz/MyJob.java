9bfd6432fe57defa8851ce716183cb63cdcbfb45
wangyafeng <wangyafeng.159@163.com>
2018-10-23 16:36:59 +0800

    修改项目api

--- src/main/java/com/art1001/supply/quartz/MyJob.java
+++ src/main/java/com/art1001/supply/quartz/MyJob.java
@@ -5,14 +5,28 @@ import org.quartz.JobDataMap;
 
 @Data
 public class MyJob {
-
+    /**
+     * 任务名称
+     */
     private String jobName;
 
+    /**
+     * 触发器组名称
+     */
     private String triggerGroupName;
 
+    /**
+     * 任务组名称
+     */
     private String jobGroupName;
 
+    /**
+     * 任务额外参数
+     */
     private JobDataMap jobDataMap;
 
+    /**
+     * 时间规则
+     */
     private String cronTime;
 }
