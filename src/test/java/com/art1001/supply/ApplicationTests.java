06ea6432b698d07e80e9c4a19cbdf2b312734bac
何少华 <heshaohua@art1001.com>
2018-10-30 10:44:53 +0800

    项目成员修改

--- src/test/java/com/art1001/supply/ApplicationTests.java
+++ src/test/java/com/art1001/supply/ApplicationTests.java
@@ -1,15 +1,22 @@
 package com.art1001.supply;
 
+import com.art1001.supply.entity.resource.ResourceEntity;
+import com.art1001.supply.entity.role.ResourcesRole;
 import com.art1001.supply.entity.tag.TagRelation;
+import com.art1001.supply.service.resource.ResourceService;
+import com.art1001.supply.service.role.ResourcesRoleService;
 import com.art1001.supply.service.tagrelation.TagRelationService;
 import com.art1001.supply.service.task.TaskService;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import javax.annotation.Resource;
+import java.time.LocalDateTime;
 import java.util.Arrays;
+import java.util.List;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -20,6 +27,13 @@ public class ApplicationTests {
 
     @Resource
     private TagRelationService tagRelationService;
+
+    @Resource
+    private ResourceService resourceService;
+
+    @Resource
+    private ResourcesRoleService resourcesRoleService;
+
     @Test
     public void testSql(){
         String tagIds=null;
@@ -34,6 +48,13 @@ public class ApplicationTests {
 
     @Test
     public void test1(){
-
+        List<ResourceEntity> resource_type = resourceService.list(new QueryWrapper<ResourceEntity>().eq("parent_id", 5));
+        resource_type.forEach(item -> {
+            ResourcesRole resourcesRole = new ResourcesRole();
+            resourcesRole.setRoleId(2);
+            resourcesRole.setCreateTime(LocalDateTime.now());
+            resourcesRole.setResourceId(item.getResourceId());
+            resourcesRoleService.save(resourcesRole);
+        });
     }
 }
