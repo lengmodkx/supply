0d6309e8edf63825a4a6e1017b70afd4367d6d34
何少华 <heshaohua@art1001.com>
2018-10-31 09:46:25 +0800

    任务的重复性

---
+++ src/main/java/com/art1001/supply/Test.java
@@ -0,0 +1,14 @@
+package com.art1001.supply;
+
+/**
+ * @author heshaohua
+ * @Title: Test
+ * @Description: TODO
+ * @date 2018/10/24 18:35
+ **/
+public class Test {
+
+    public static void main(String[] args) {
+        System.out.println("cao ni ma !");
+    }
+}
