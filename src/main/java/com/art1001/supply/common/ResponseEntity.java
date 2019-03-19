bbd0a067556af4d5900f2ca2a36e897c367e2dfa
小钉铛 <15712904437@163.com>
2019-03-19 16:26:03 +0800

    添加数据返回格式规范类

---
+++ src/main/java/com/art1001/supply/common/ResponseEntity.java
@@ -0,0 +1,69 @@
+package com.art1001.supply.common;
+
+import com.alibaba.fastjson.JSONObject;
+import lombok.Data;
+
+/**
+ * @Author: heshaohua
+ * @Description: 封装返回信息实体类
+ */
+@Data
+public class ResponseEntity {
+    protected boolean success; // 是否成功
+    protected String errmsg; // 错误内容
+    protected Integer errcode; // 错误代码
+    protected Object data; // 数据对象
+
+    /**
+     * @param success
+     * @param data
+     * @param errcode
+     * @param errmsg
+     * @author by chippy
+     * @desc 构造函数.
+     */
+    public ResponseEntity(boolean success, Object data, Integer errcode, String errmsg) {
+        this.success = success;
+        this.data = data;
+        this.errcode = errcode;
+        this.errmsg = errmsg;
+    }
+
+    /**
+     * @param errcode
+     * @param errmsg
+     * @author by chippy
+     * @desc 构造函数.
+     */
+    public ResponseEntity(Integer errcode, String errmsg) {
+        this.success = false;
+        this.data = null;
+        this.errcode = errcode;
+        this.errmsg = errmsg;
+    }
+
+    /**
+     * @param data
+     * @author by chippy
+     * @desc 构造函数.
+     */
+    public ResponseEntity(Object data) {
+        this.success = true;
+        this.data = data;
+    }
+
+    /**
+     * @return
+     * @author by chippy
+     * @desc 将ResponseEntity转换成json格式对象.
+     */
+    public Object toJson() {
+        return JSONObject.toJSONString(this);
+    }
+
+    public boolean isSuccess() {
+        return success;
+    }
+
+
+}
