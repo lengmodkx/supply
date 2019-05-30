e55f457d75a1e7b2c9ea22c021b64962c53c7b59
小钉铛 <15712904437@163.com>
2019-05-30 14:09:41 +0800

    1.文件提交

---
+++ src/main/java/com/art1001/supply/mapper/resource/ResourceRoleBindTemplateMapper.java
@@ -0,0 +1,16 @@
+package com.art1001.supply.mapper.resource;
+
+import com.art1001.supply.entity.resource.ResourceRoleBindTemplate;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * <p>
+ *  Mapper 接口
+ * </p>
+ *
+ * @author heshaohua
+ * @since 2019-05-29
+ */
+public interface ResourceRoleBindTemplateMapper extends BaseMapper<ResourceRoleBindTemplate> {
+
+}
