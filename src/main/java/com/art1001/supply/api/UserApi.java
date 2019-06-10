4200ffb81ec90ca699801078c8128accac7b9fd1
yanglujing <yanglujing@art1001.com>
2019-06-10 13:23:40 +0800

    用户详情获取

--- src/main/java/com/art1001/supply/api/UserApi.java
+++ src/main/java/com/art1001/supply/api/UserApi.java
@@ -282,7 +282,7 @@ public class UserApi {
     /**
      * 获取用户信息
      */
-    @PostMapping("/getUserInfo/{userId}")
+    @GetMapping("/getUserInfo/{userId}")
     public  JSONObject  getUserInfo(@PathVariable String userId){
         JSONObject jsonObject=new JSONObject();
         try {
