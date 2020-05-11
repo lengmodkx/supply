2798f55a63ad8fd6952b6a690553bf6b6525dc17
汪亚锋 <admin@example.com>
2020-05-11 14:55:07 +0800

    修改文件

--- src/main/java/com/art1001/supply/annotation/ProjectAuth.java
+++ src/main/java/com/art1001/supply/annotation/ProjectAuth.java
@@ -10,7 +10,7 @@ import java.lang.annotation.*;
 @Documented
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.METHOD)
-public @interface ProAuthentization {
+public @interface ProjectAuth {
 
     String value();
 }
