fedde28248e78bac91417921da927e5dc6aaa9b7
dengkaixin <dengkaixin@art1001.com>
2020-11-06 15:54:12 +0800

    修改大资源表

--- src/main/java/com/art1001/supply/listener/InitializationListener.java
+++ src/main/java/com/art1001/supply/listener/InitializationListener.java
@@ -1,5 +1,5 @@
 /**
- * 
+ *
  */
 package com.art1001.supply.listener;
 
@@ -35,58 +35,49 @@ import java.util.stream.Collectors;
 @Component
 public class InitializationListener implements ApplicationListener<ContextRefreshedEvent> {
 
-	@Resource
-	private RedisUtil redisUtil;
+    @Resource
+    private RedisUtil redisUtil;
 
-	@Resource
-	private ResourceService resourceService;
+    @Resource
+    private ResourceService resourceService;
 
-	@Resource
-	private ProResourcesService proResourcesService;
-
-
-	private static final Logger logger = LoggerFactory.getLogger(InitializationListener.class);
-
-	private SchedulerFactory schedulerFactory = new StdSchedulerFactory();
-	/* (non-Javadoc)
-	 * @see org.springframework.context.ApplicationListener#onApplicationEvent(org.springframework.context.ApplicationEvent)
-	 */
-	@Override
-	public void onApplicationEvent(ContextRefreshedEvent event) {
-		if (null == event.getApplicationContext().getParent()) {
-
-			List<String> allResources = redisUtil.getList(String.class, "allResources");
-			if(CollectionUtils.isEmpty(allResources)){
-				logger.info("redis中没有相应的权限key信息,需要从数据库查询......");
-				//获取所有资源的kay
-				List<ResourceEntity> allResourceList = resourceService.list(new QueryWrapper<ResourceEntity>().lambda().ne(ResourceEntity::getResourceLevel, 1));
-				allResources = allResourceList.stream().map(ResourceEntity::getResourceKey).collect(Collectors.toList());
-
-				List<ProResources> proResourcesList = proResourcesService.list(new QueryWrapper<ProResources>().lambda().ne(ProResources::getSLevel, 1));
-				List<String> proKeyList = proResourcesList.stream().map(ProResources::getSSourceKey).collect(Collectors.toList());
-				allResources.addAll(proKeyList);
-				redisUtil.lset("allResources", allResources);
-				logger.info("所有资源key信息查询完毕,存储进redis.");
-			}
-
-			/**
-			 * 这里是quartz任务调度器的启动  删了就不行了
-			 */
-			try {
-				Scheduler scheduler = schedulerFactory.getScheduler();
-				if(scheduler.isStarted()){
-					logger.info("scheduler  xxxxx ");
-				}else{
-					scheduler.start();
-				}
-			} catch (SchedulerException e) {
-				e.printStackTrace();
-			}
+    @Resource
+    private ProResourcesService proResourcesService;
 
 
+    private static final Logger logger = LoggerFactory.getLogger(InitializationListener.class);
 
-			logger.info("System initialization success.");
-		}
-	}
+    private SchedulerFactory schedulerFactory = new StdSchedulerFactory();
+
+    /* (non-Javadoc)
+     * @see org.springframework.context.ApplicationListener#onApplicationEvent(org.springframework.context.ApplicationEvent)
+     */
+    @Override
+    public void onApplicationEvent(ContextRefreshedEvent event) {
+        if (null == event.getApplicationContext().getParent()) {
+
+            List<String> allResources = redisUtil.getList(String.class, "allResources");
+            redisUtil.lset("allResources", allResources);
+            logger.info("所有资源key信息查询完毕,存储进redis.");
+
+
+            /**
+             * 这里是quartz任务调度器的启动  删了就不行了
+             */
+            try {
+                Scheduler scheduler = schedulerFactory.getScheduler();
+                if (scheduler.isStarted()) {
+                    logger.info("scheduler  xxxxx ");
+                } else {
+                    scheduler.start();
+                }
+            } catch (SchedulerException e) {
+                e.printStackTrace();
+            }
+
+
+            logger.info("System initialization success.");
+        }
+    }
 
 }
