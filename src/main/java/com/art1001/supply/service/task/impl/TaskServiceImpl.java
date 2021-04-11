7b89116f333f9c293dceda8f68dd8e9e56423329
Administrator <admin@example.com>
2021-04-11 16:20:54 +0800

    修改任务完成

--- src/main/java/com/art1001/supply/service/task/impl/TaskServiceImpl.java
+++ src/main/java/com/art1001/supply/service/task/impl/TaskServiceImpl.java
@@ -1556,8 +1556,8 @@ public class TaskServiceImpl extends ServiceImpl<TaskMapper, Task> implements Ta
     public void completeTask(Task task) {
         //判断当前完成的是不是子任务 如果是则查询出这个子任务的同级其他子任务并且判断是否全部完成 如果全部完成 标记父任务的 "subIsAllComplete" 属性为true
         if(!StringUtils.equalsIgnoreCase(task.getParentId(),"0")){
-            Task one = getOne(new QueryWrapper<Task>().eq("parent_id", task.getParentId()).eq("task_status", false));
-            if (one == null) {
+            int one = count(new QueryWrapper<Task>().eq("parent_id", task.getParentId()).eq("task_status", false));
+            if (one >=1) {
                 Task pTask = new Task();
                 pTask.setTaskId(task.getParentId());
                 pTask.setUpdateTime(System.currentTimeMillis());
